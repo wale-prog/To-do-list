e4089e81f6b47468562a327cab52824c1c5e7f71
wale-prog <olapetanwale@outlook.com>
2022-05-05 03:33:43 +0100

    Add dynamic js codes and correct linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,36 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-       index: './src/index.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: './src/index.html',
-        }),
-      ],
-    output: {
-        filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    // optimization: {
-    //     runtimechunk: 'single', // This block of code can be use when there is more than one entry point, in cases where code splitting is used.
-    // },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-  };
+    ],
+  },
+};
